2fb7f74868478a950480a74aeb315d120ef2650c
Prisca Joseph <priscajoseph@priscas-mbp.myfiosgateway.com>
2020-07-22 01:31:42 -0400

    First Commit

---
+++ server/render.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import { StaticRouter, matchPath } from 'react-router-dom';
+import Page from '../src/Page.jsx';
+import template from './template.js';
+import store from '../src/store.js';
+import routes from '../src/routes.js';
+
+async function render(req, res) {
+  const activeRoute = routes.find(
+    route => matchPath(req.path, route),
+  );
+
+  let initialData;
+  if (activeRoute && activeRoute.component.fetchData) {
+    const match = matchPath(req.path, activeRoute);
+    const index = req.url.indexOf('?');
+    const search = index !== -1 ? req.url.substr(index) : null;
+    initialData = await activeRoute.component.fetchData(match, search, req.headers.cookie);
+  }
+
+  const userData = await Page.fetchData(req.headers.cookie);
+
+  store.initialData = initialData;
+  store.userData = userData;
+  const element = (
+    <StaticRouter location={req.url} context={{}}>
+      <Page />
+    </StaticRouter>
+  );
+  const body = ReactDOMServer.renderToString(element);
+  res.send(template(body, initialData, userData));
+}
+
+export default render;
